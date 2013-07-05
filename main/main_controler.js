11821e4b6bda72b31933311584023ae9bdafa881
Rafael <rafaelox@cubcode.com>
2013-07-05 13:27:07 -0500

    new
    ready

---
+++ main/main_controler.js
@@ -0,0 +1,11 @@
+function TitleCtrl($scope) {
+  $scope.name = 'World';
+}
+
+function CompanyCtrl($scope) {
+  $scope.name = 'Worldssssss';
+}
+
+function LoginCtrl($scope) {
+  $scope.name = 'Worldss';
+}
